efc9e697b81af1e6037edbf0058f456479611713
YN <lobsterfold@gmail.com>
2026-02-10 22:40:41 +0800

    fix：修复了Aspire指标(Metrics)显示问题

--- OctopusEx.WebCore/Extensions/AspireExtensions.cs
+++ OctopusEx.WebCore/Extensions/AspireExtensions.cs
@@ -7,6 +7,9 @@ using OpenTelemetry.Trace;
 
 namespace OctopusEx.WebCore.Extensions;
 
+using OpenTelemetry;
+using OpenTelemetry.Metrics;
+
 public static class AspireExtensions
 {
     public static IHostApplicationBuilder AddAspireOpenTelemetry(this IHostApplicationBuilder builder)
@@ -17,13 +20,26 @@ public static class AspireExtensions
 
     private static IHostApplicationBuilder ConfigureOpenTelemetry(this IHostApplicationBuilder builder)
     {
-
         builder.Services.AddOpenTelemetry()
             .ConfigureResource(resource => resource
                 .AddService(serviceName: builder.Environment.ApplicationName,
                     serviceVersion: typeof(AspireExtensions).Assembly.GetName().Version?.ToString() ?? "1.0.0",
                     serviceInstanceId: Environment.MachineName)
-                )
+            )
+            .WithMetrics(metrics =>
+            {
+                metrics.AddAspNetCoreInstrumentation()
+                    .AddHttpClientInstrumentation()
+                    .AddOtlpExporter(options =>
+                    {
+                        var otlpEndpoint = builder.Configuration["OTEL_EXPORTER_OTLP_ENDPOINT"];
+                        if ( !String.IsNullOrWhiteSpace(otlpEndpoint) )
+                        {
+                            options.Endpoint = new Uri(otlpEndpoint);
+                        }
+                    });
+
+            })
             .WithTracing(tracing =>
             {
                 tracing
@@ -48,13 +64,20 @@ public static class AspireExtensions
             });
 
 
+        // var useOtlpExporter = !string.IsNullOrWhiteSpace(builder.Configuration["OTEL_EXPORTER_OTLP_ENDPOINT"]);
+        // Console.WriteLine($"[ServiceDefaults] OTLP exporter enabled: {useOtlpExporter}, endpoint: {builder.Configuration["OTEL_EXPORTER_OTLP_ENDPOINT"]}");
+        //
+        // if (useOtlpExporter)
+        // {
+        //     builder.Services.AddOpenTelemetry().UseOtlpExporter();
+        // }
+        
         // 关键：为日志配置相同的资源构建器
         builder.Logging.AddOpenTelemetry(logging =>
         {
-            logging.AddOtlpExporter(); // Aspire will automatically configure the endpoint
+            logging.AddOtlpExporter();// Aspire will automatically configure the endpoint
         });
 
         return builder;
     }
-
 }
